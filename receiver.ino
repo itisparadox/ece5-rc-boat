5c250efbd358ee77065d3198745f653a59330a1b
Jack Driscoll-Natale <jackdrna@JACKDRNA.>
2025-03-12 10:19:19 -0700

    FINALLY

--- receiver.ino
+++ receiver.ino
@@ -5,20 +5,19 @@
 
 // defines RF24 object (from RF24.h)
 // (CE PIN, CSN PIN)
-RF24 receiver(9,10);
+RF24 radio(7,8);
 
 // for linking transmitter and receiver (must be same)
 const byte address[6] = "00001";
 
 void setup() {
 	
-	Sierial.begin(9600);
+	Serial.begin(9600);
 	while(!Serial) {} // wait for serial
 
-	if (!radio.begin()) { // initialize
-		Serial.println("radio hardware not responding!!");
-		while(1) {} // hold an infinite loop
-	}
+	bool begin = radio.begin();
+    if (begin) Serial.println("Begin Successful");
+    else Serial.println("Begin Not Succesful");	
 
 	radio.setPALevel(RF24_PA_LOW); // low power for test
 	radio.setPayloadSize(sizeof(payload));
@@ -27,16 +26,10 @@ void setup() {
 }
 
 void loop() {
-
-	uint8_t pipe;
-    if (radio.available(&pipe)) {              // is there a payload? get the pipe number that received it
-    	uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
-      	radio.read(&payload, bytes);             // fetch payload from FIFO
-      	Serial.print("Received ");
-      	Serial.print(bytes);  // print the size of the payload
-      	Serial.print(" bytes on pipe ");
-      	Serial.print(pipe);  // print the pipe number
-      	Serial.print(": ");
-      	Serial.println(payload);  // print the payload's value
+	 if (radio.available()) {              // is there a payload? get the pipe number that received it
+        uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
+        radio.read(&payload, bytes);             // fetch payload from FIFO
+        Serial.print("Received ");
+        Serial.println(payload);  // print the payload's value
     }
 }
--- transmitter.ino
+++ transmitter.ino
@@ -5,7 +5,7 @@
 
 // initializes radio object
 // (CE PIN, CSN PIN)
-RF24 radio(9, 10);
+RF24 radio(7, 8);
 
 // pipeline address (must be identical to other transceiver)
 const byte address[6] = "00001";
@@ -18,10 +18,9 @@ void setup() {
 	
 	while(!Serial) {} // wait for serial to start
 
-	if (!radio.begin()) {
-    	Serial.println("radio hardware is not responding!!");
-    	while (1) {}  // hold in infinite loop
-  	}
+	bool begin = radio.begin();
+    if (begin) Serial.println("Begin Successful");
+    else Serial.println("Begin NOT Succesful.");
 
 	// set low for testing
 	radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
@@ -44,7 +43,6 @@ void loop() {
      	Serial.print("Transmission successful! ");  // payload was delivered
       	Serial.print("Sent: ");
       	Serial.println(payload);  // print payload sent
-      	payload += 0.01;          // increment float payload
     } else {
       	Serial.println("Transmission failed or timed out");  // payload was not delivered
     }
